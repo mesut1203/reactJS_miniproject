5eca1053f27dcbefe70e74ddd9f95e6b5c4493a9
Khai <caubevang2004@gmail.com>
2025-10-23 17:40:50 +0700

    Init project

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+export default function App() {
+    return (
+        <div className="text-center">
+            <Button className="bg-red-500 hover:bg-green-400">Click me</Button>
+        </div>
+    );
+}
